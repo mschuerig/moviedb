8d743fd5dfc1551b713f5be2ed5c0bc29791cc56
Michael Schuerig <michael@schuerig.de>
2009-05-24 13:08:42 +0200

    Movie: extracted concerns.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,69 +1,12 @@
 class Movie < ActiveRecord::Base
+  concerned_with :roles
+
   attr_protected :release_year
 
   acts_as_xapian :texts => [ :title, :summary ],
     :values => [ [ :release_year, 1, 'year', :number ] ]
 
   validates_presence_of :title
-
-  has_many :roles, :dependent => :destroy, :extend => RoleTypeAssociationExtensions
-
-  module ParticipantTypeExtensions
-    include RoleTypeAssociationExtensions
-
-    ### TODO how to ensure that the new participant is seen before saving?
-    def add(role_name, person, options = {})
-      proxy_owner.roles.build(
-        :person => person,
-        :role_type => RoleType[role_name],
-        :credited_as => options[:as]
-      )
-    end
-
-    def remove(role_name, person)
-      role = Role.find(:first,
-        :joins => :role_type,
-        :conditions => {
-          :person_id => person,
-          :movie_id  => proxy_owner,
-          :role_types => { :name => role_name.to_s }
-        })
-      proxy_owner.roles.delete(role)
-    end
-
-    def replace(role, refs_with_options)
-      transaction do
-        options = refs_with_options.map { |o| o.kind_of?(Hash) ? o.dup.delete(:id) : {} }
-        others  = refs_with_options.map { |o| o.kind_of?(Hash) ? o[:id] : o }
-
-        others  = Person.find(others)
-        current = proxy_owner.participants.as(role)
-
-        obsolete = current.select { |o| !others.include?(o) }
-        fresh    = others.zip(options).select { |o, opts| !current.include?(o) }
-
-        obsolete.each { |o|       self.remove(role, o) }
-        fresh.each    { |o, opts| self.add(role, o, opts) }
-      end
-    end
-
-    RoleType.each_name do |name|
-      class_eval <<-END
-        def add_#{name}(person, options = {})
-          add(:#{name}, person, options)
-        end
-        def remove_#{name}(person)
-          remove(:#{name}, person)
-        end
-        def replace_#{name.pluralize}(others)
-          replace(:#{name}, others)
-        end
-      END
-    end
-  end
-
-  has_many :participants, :through => :roles, :source => :person, :extend => ParticipantTypeExtensions
-
   has_and_belongs_to_many :awardings, :after_remove => lambda { |_, awarding| awarding.destroy }
 
   default_scope :order => 'title, release_date'
@@ -97,18 +40,7 @@ class Movie < ActiveRecord::Base
     end
   end
 
-
-  RoleType.each_name do |role_name|
-    define_method(role_name.pluralize) do
-      participants.as(role_name)
-    end
-
-    define_method("#{role_name.pluralize}=") do |others|
-      participants.replace(role_name, others)
-    end
-  end
-
-  def before_save
+  def before_validation
     self.release_year = release_date.blank? ? nil : release_date.year
   end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,29 +1,7 @@
 class Person < ActiveRecord::Base
-  concerned_with :awards, :name, :roles, :awards
+  concerned_with :awards, :name, :roles, :social
   
   default_scope :order => 'lastname, firstname, serial_number'
-
-  def coworkers() #options = {})
-=begin
-    if movie = options[:movie]
-      Person.scoped(
-        :conditions => [
-          "people.id <> ? AND people.id IN" +
-          " (SELECT roles.person_id FROM roles WHERE roles.movie_id = ?)",
-          id, movie
-        ]
-      )
-    else
-=end
-      Person.scoped(
-        :conditions => [
-        "people.id <> ? AND people.id IN" +
-        " (SELECT roles.person_id FROM roles WHERE roles.movie_id IN" +
-        " (SELECT roles.movie_id from roles WHERE roles.person_id = ?))",
-        id, id]
-      )
-#    end
-  end
 
   def self.find(*args)
     args = args.dup
