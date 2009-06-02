b7535f9b1a59aa82ab3f5f7bee68d48ba5a1a70c
Michael Schuerig <michael@schuerig.de>
2009-06-02 23:07:36 +0200

    Added an (ineffectual) edit button to items in the character/actor list. Some heavy-handed styling.

---
+++ public/javascripts.development/moviedb/nls/common.js
@@ -0,0 +1,3 @@
+{
+  editItem: 'Edit'
+}
--- public/javascripts.development/moviedb/ui/ActorItem.js
+++ public/javascripts.development/moviedb/ui/ActorItem.js
@@ -1,8 +1,16 @@
 dojo.provide('moviedb.ui.ActorItem');
 dojo.require('dijit.InlineEditBox');
+dojo.require('dijit.form.Button');
 dojo.require('moviedb.ui.PersonItem');
+dojo.require('dojo.i18n');
+dojo.requireLocalization('moviedb', 'common');
 
 dojo.declare('moviedb.ui.ActorItem', moviedb.ui.PersonItem, {
   templatePath: dojo.moduleUrl('moviedb', 'ui/_ActorItem/ActorItem.html'),
-  widgetsInTemplate: true
+  widgetsInTemplate: true,
+
+  postMixInProperties: function() {
+    this._nls = dojo.i18n.getLocalization('moviedb', 'common');
+    this.inherited(arguments);
+  }
 });
